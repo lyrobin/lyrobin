a0269c9dac9904ebe0637f98b14ff44183ce4318
Benno Lin <blueworrybear@gmail.com>
2024-07-05 10:44:32 +0800

    test analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import {
   NavigationEnd,
@@ -11,6 +11,7 @@ import { AngularIconComponent } from './components/icons/angular-icon.component'
 import { FirebaseIconComponent } from './components/icons/firebase-icon.component';
 import { ArrowBackIconComponent } from './components/icons/arrow-back-icon.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Analytics, logEvent } from '@angular/fire/analytics';
 
 @Component({
   selector: 'app-root',
@@ -26,13 +27,19 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private readonly router = inject(Router);
   private readonly isMainPage$ = this.router.events.pipe(
     filter((event): event is NavigationEnd => event instanceof NavigationEnd),
     map((event: NavigationEnd) => event.url === '/'),
     startWith(true)
   );
+
+  constructor(private analytics: Analytics) {}
+  ngOnInit(): void {
+    console.log(this.analytics.app);
+    logEvent(this.analytics, 'page_view');
+  }
 
   isMainPage = toSignal(this.isMainPage$);
 }
